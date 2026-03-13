8629a6b30fe28e0cd30a0ac4eda5a448d61ab1fe
anilkumar663 <dasaniya.anilkumar@gmail.com>
2026-03-13 14:16:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ terraform {
 provider "azurerm" {
     features {}
     resource_provider_registrations = "none"
-    subscription_id = "84fffe9e-21a8-4f72-93c2-471d1c785dba"
+    subscription_id = ""
 }
